376e90cdc9916a46c78577dde13db7267335888a
tahirakram15 <tahirakram15@hotmail.com>
2015-07-08 21:58:31 +0500

    hello

---
+++ AtomCMS/template/navigation.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-default" role= "navigation">
+		
+			<div class="container">
+				<ul class="nav navbar-nav">
+					<li <?php if($pageid==1){echo 'class="active"';}?>><a href = "?page=1">Home</a></li>
+					<li <?php if($pageid==2){echo 'class="active"';}?>><a href = "?page=2">About Us</a></li>
+					<li><a href = "#">Faq</a></li>
+					<li><a href = "#">Contact US</a></li>
+				</ul>
+			</div>		
+		
+</nav>
